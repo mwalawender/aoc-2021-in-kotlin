0d03c09b9b4cd2d9a45b8caf58d40457fc8e3635
Michal Walawender <mwalawen@akamai.com>
2021-12-01 18:16:01 +0100

    AOC-2021-Day1

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,15 +1,17 @@
 fun main() {
-    fun part1(input: List<String>): Int {
-        return input.size
-    }
+    fun part1(input: List<String>) =
+        input.map(String::toInt)
+            .zipWithNext()
+            .count { measurement -> measurement.second > measurement.first }
 
-    fun part2(input: List<String>): Int {
-        return input.size
-    }
+    fun part2(input: List<String>) =
+        input.map(String::toInt)
+            .windowed(3, 1, false)
+            .zipWithNext()
+            .count { threeMeasurement ->
+                threeMeasurement.second.sum() > threeMeasurement.first.sum()
+            }
 
-    // test if implementation meets criteria from the description, like:
-    val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
 
     val input = readInput("Day01")
     println(part1(input))
